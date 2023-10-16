59ba162e875f794a4d7a1feb7d655f73b48838dd
bosscal <gbosscal@gmail.com>
2023-10-16 16:14:15 +0800

    fix: can not build a new uuid for id

--- model/base.py
+++ model/base.py
@@ -9,7 +9,12 @@ Base = declarative_base()
 class BaseModel(Base):
     __abstract__ = True  # 声明这是一个抽象基类，不会创建对应的数据库表格
 
-    id = Column(VARCHAR(36), primary_key=True, default=str(uuid.uuid4()))
+    @staticmethod
+    def get_new_uuid():
+        data = uuid.uuid4()
+        return str(data)
+
+    id = Column(VARCHAR(36), primary_key=True, default=get_new_uuid())
     created_time = Column(DateTime, default=datetime.now)
     updated_time = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     is_deleted = Column(Boolean, default=False)
