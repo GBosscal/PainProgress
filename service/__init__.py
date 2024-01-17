aa565010b035b10670c5b3ea3844735d16b049aa
bosscal <gbosscal@gmail.com>
2024-01-17 11:18:09 +0800

    feat: add url_prefix=pain.

--- service/__init__.py
+++ service/__init__.py
@@ -1,4 +1,4 @@
-from sanic import Sanic
+from sanic import Sanic, Blueprint
 from textwrap import dedent
 
 
@@ -20,13 +20,20 @@ def create_app():
     from view.login import login_blueprint
     from view.pain_data import pain_with_image_blueprint
 
-    app.blueprint(user_blueprint)
-    app.blueprint(feedback_blueprint)
-    app.blueprint(storage_blueprint)
-    app.blueprint(pain_blueprint)
-    app.blueprint(hospital_blueprint)
-    app.blueprint(login_blueprint)
-    app.blueprint(pain_with_image_blueprint)
+    all_blueprint = Blueprint.group(
+        user_blueprint, feedback_blueprint,
+        storage_blueprint, pain_blueprint, hospital_blueprint,
+        login_blueprint, pain_with_image_blueprint,
+        url_prefix="/pain"
+    )
+    app.blueprint(all_blueprint)
+    # app.blueprint(user_blueprint)
+    # app.blueprint(feedback_blueprint)
+    # app.blueprint(storage_blueprint)
+    # app.blueprint(pain_blueprint)
+    # app.blueprint(hospital_blueprint)
+    # app.blueprint(login_blueprint)
+    # app.blueprint(pain_with_image_blueprint)
 
     # 修改apidoc的定义
     app.ext.openapi.describe(
